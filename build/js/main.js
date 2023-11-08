6bcec18cdb0fc1fc68c5100879136e143d5f2101
ankit1519 <raazankit1502@gmail.com>
2023-11-08 14:35:13 +0530

    explicit type

--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,8 @@
 "use strict";
 let username = "Ankit raj";
 console.log(username);
+let a = 12;
+let b = 6;
+let c = 2;
+console.log(a / b);
+console.log(c * b);
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,10 @@
 let username="Ankit raj"
 console.log(username);
+
+
+let a:number=12
+let b:number=6
+let c:number=2
+
+console.log(a/b);
+console.log(c*b);
