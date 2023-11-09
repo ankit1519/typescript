88973214a5c96729f13b2638d27fb4ccdad12bae
ankit1519 <raazankit1502@gmail.com>
2023-11-09 06:01:04 +0530

    class,interface, getter setter, static

--- build/js/05_classes.js
+++ build/js/05_classes.js
@@ -30,5 +30,54 @@ class WebDev extends Coder {
 }
 const Raj = new WebDev('Asus', "Raj", 'SN', 22);
 console.log(Raj.getLang());
-//console.log(Raj.age);
-//
+class Guitarists {
+    constructor(name, instrument) {
+        this.name = name;
+        this.instrument = instrument;
+    }
+    play(action) {
+        return `${this.name} ${action} ${this.instrument}`;
+    }
+}
+const AnkitRaj = new Guitarists("Ankit", 'guitar');
+console.log(AnkitRaj.play('strums'));
+//static
+class Beauty {
+    constructor(name) {
+        this.name = name;
+        this.name = name;
+        this.id = ++Beauty.count;
+    }
+}
+Beauty.count = 0;
+const AkankshaJ = new Beauty("Akanksha Jha");
+const AkankshaJha = new Beauty("Akanksha");
+const AnkitR = new Beauty("Ankit Raj");
+const AnkitSri = new Beauty("Ankit srivastava");
+console.log(Beauty.count);
+console.log(AkankshaJ.id);
+console.log(AnkitSri.id);
+console.log(AkankshaJha.id);
+//Getters and setters
+class Arr {
+    constructor() {
+        this.arrOfStr = [];
+    }
+    get data() {
+        return this.arrOfStr;
+    }
+    set data(value) {
+        if (Array.isArray(value) && value.filter((val) => typeof val === 'string').length >= 1) {
+            this.arrOfStr = value;
+            return;
+        }
+        else {
+            throw new Error("param is not array of string");
+        }
+    }
+}
+const newArr = new Arr();
+newArr.data = ['ankit', 'raj', 'aj'];
+console.log(newArr.data);
+newArr.data = [...newArr.data, 'Akanksha Ji'];
+console.log(newArr.data);
--- src/05_classes.ts
+++ src/05_classes.ts
@@ -47,4 +47,74 @@ console.log(Raj.getLang());
 //console.log(Raj.age);
 
 
-//
+//----------INTERFACE----------------------
+interface Musicians{
+    name:string,
+    instrument:string,
+    play(action:string):string
+}
+
+class Guitarists implements Musicians{
+    name:string
+    instrument: string
+    constructor(name:string,instrument:string){
+        this.name=name
+        this.instrument=instrument
+    }
+    play(action:string){
+        return `${this.name} ${action} ${this.instrument}`
+    }
+}
+
+const AnkitRaj=new Guitarists("Ankit",'guitar')
+console.log(AnkitRaj.play('strums'))
+
+
+//static
+class Beauty{
+    public static count:number=0;
+    id:number
+    constructor(public name:string){
+        this.name=name
+        this.id= ++Beauty.count
+    }
+}
+
+const AkankshaJ=new Beauty("Akanksha Jha")
+const AkankshaJha=new Beauty("Akanksha")
+const AnkitR=new Beauty("Ankit Raj")
+const AnkitSri=new Beauty("Ankit srivastava")
+console.log(Beauty.count);
+console.log(AkankshaJ.id)
+console.log(AnkitSri.id)
+console.log(AkankshaJha.id)
+
+//Getters and setters
+class Arr{
+    arrOfStr:string[]
+    constructor(){
+        this.arrOfStr=[]
+    }
+    public get data():string[]{
+        return this.arrOfStr
+    }
+
+    public set data(value:string[]){
+        if(Array.isArray(value) && value.filter((val)=>typeof val==='string').length>=1){
+            this.arrOfStr=value
+            
+            
+            return
+        }
+        else{
+            throw new Error("param is not array of string");
+            
+        }
+    }
+}
+
+const newArr=new Arr()
+newArr.data=['ankit','raj','aj']
+console.log(newArr.data);
+newArr.data=[...newArr.data,'Akanksha Ji']
+console.log(newArr.data);
